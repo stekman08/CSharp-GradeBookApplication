adfe12476d7feea72b83fc7fdcfcf3a7043f7845
stekman08 <38240615+stekman08@users.noreply.github.com>
2018-04-11 10:36:19 +0200

    Add files via upload

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
             {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             }
@@ -21,7 +21,7 @@ namespace GradeBook.GradeBooks
             int threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if(grades[threshold-1] <= averageGrade)
+            if (grades[threshold - 1] <= averageGrade)
             {
                 return 'A';
             }
@@ -29,19 +29,38 @@ namespace GradeBook.GradeBooks
             {
                 return 'B';
             }
-            else if(grades[(threshold * 3) - 1] <= averageGrade)
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
             {
                 return 'C';
             }
             else if (grades[(threshold * 4) - 1] <= averageGrade)
             {
                 return 'D';
-            }          
+            }
             else
             {
                 return 'F';
             }
+        }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
