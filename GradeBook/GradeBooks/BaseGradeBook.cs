bf71c1efef2e2270d02662d099117c678d8e2f01
stekman08 <38240615+stekman08@users.noreply.github.com>
2018-04-11 13:35:18 +0200

    Add files via upload

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,13 +13,16 @@ namespace GradeBook.GradeBooks
 
     public abstract class BaseGradeBook
     {
+        
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
