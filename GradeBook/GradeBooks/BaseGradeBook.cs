2aa17cb57e1860033f366a27d7c3ca792cfcf608
stekman08 <38240615+stekman08@users.noreply.github.com>
2018-04-11 14:31:56 +0200

    Add files via upload

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -112,18 +112,24 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int plus = 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                plus = 1;
+            }
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + plus;
                 case 'B':
-                    return 3;
+                    return 3 + plus;
                 case 'C':
-                    return 2;
+                    return 2 + plus;
                 case 'D':
-                    return 1;
+                    return 1 + plus;
             }
-            return 0;
+            return 0 + plus;
         }
 
         public virtual void CalculateStatistics()
